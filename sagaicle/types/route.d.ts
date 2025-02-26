908942831a7b93fd99626f41cd1fb78b1f58f2b7
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-02-26 12:27:15 +0900

    feat: 🍻 JSON types

---
+++ sagaicle/types/route.d.ts
@@ -0,0 +1,15 @@
+// Definition of the Route structure
+
+type Route = {
+  id: string;
+  title: string;
+  description: string;
+  distance: number;
+  time: number;
+  tags: string[];
+  likes: number;
+  image: string;
+  update: string;
+};
+
+export default Route;
